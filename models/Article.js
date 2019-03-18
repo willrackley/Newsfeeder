16311fa8308ca5c828cd90f677eaf3a2b8b7f1fa
williamrackley <willrackley65@gmail.com>
2019-03-18 11:13:22 -0400

    added another site to scrape and route

--- models/Article.js
+++ models/Article.js
@@ -16,7 +16,15 @@ var ArticleSchema = new Schema({
     type: String,
     required: true
   },
-  // `note` is an object that stores a Note id
+  category: {
+    type: String,
+    required: true
+  },
+  dateAdded: {
+    type: Date,
+    default: Date.now
+  },
+  // `comment` is an object that stores a Note id
   // The ref property links the ObjectId to the Note model
   // This allows us to populate the Article with an associated Note
   comment: {
--- routes/scrape-route.js
+++ routes/scrape-route.js
@@ -4,7 +4,7 @@ var axios = require("axios");
 var cheerio = require("cheerio");
 var router = express.Router();
 
-router.get("/", function(req, res) {
+router.get("/sports", function(req, res) {
     // First, we grab the body of the html with axios
     axios.get("https://www.cbssports.com/").then(function(response) {
      
@@ -20,24 +20,92 @@ router.get("/", function(req, res) {
           .text().trim();
         result.link = 'https://www.cbssports.com/' + $(this)
           .attr("href");
-          //console.log(result);
-        //Create a new Article using the `result` object built from scraping
-        db.Article.create(result)
-          .then(function(dbArticle) {
-            // View the added result in the console
-            console.log(dbArticle);
-          })
-          .catch(function(err) {
-            // If an error occurred, log it
-            console.log(err);
-          });
-    
+        result.category = "sports"
+          
+        //console.log(result);
+        
+        db.Article.find({
+            title: result.title
+        }, function(err, data) {
+            // Log any errors if the server encounters one
+            if (err) {
+              console.log(err);
+            }
+            //checks to see if article is already in the database
+            //if it isnt then we add it
+            if (data.length === 0) {
+                 //Create a new Article using the `result` object built from scraping
+                 db.Article.create(result)
+                 .then(function(dbArticle) {
+                   // View the added result in the console
+                   console.log(dbArticle);
+                 })
+                 .catch(function(err) {
+                   // If an error occurred, log it
+                   console.log(err);
+                 }); 
+            }
+            //if the scraped article is in the database then we end the res
+            if (data.length !== 0) {
+                res.end();
+            }
+        });
     });
-  
       // Send a message to the client
-      res.send("Scrape Complete");
+      res.send("Sports Scrape Complete");
     });
   });
+
+  router.get("/entertainment", function(req, res) {
+    // First, we grab the body of the html with axios
+    axios.get("https://www.etonline.com/news").then(function(response) {
+     
+      var $ = cheerio.load(response.data);
+  
+     
+      $("div.item-list ul li article h2 a").each(function(i, element) {
+        
+        var eResult = {};
+  
+        eResult.title = $(this)
+          .text().trim();
+        eResult.link = 'https://www.etonline.com' + $(this)
+          .attr("href");
+        eResult.category = "entertainment" 
+       console.log(eResult);
+        
+        db.Article.find({
+            title: eResult.title
+        }, function(err, data) {
+            // Log any errors if the server encounters one
+            if (err) {
+              console.log(err);
+            }
+            //checks to see if article is already in the database
+            //if it isnt then we add it
+            if (data.length === 0) {
+                 //Create a new Article using the `result` object built from scraping
+                 db.Article.create(eResult)
+                 .then(function(dbArticle) {
+                   // View the added result in the console
+                   console.log(dbArticle);
+                 })
+                 .catch(function(err) {
+                   // If an error occurred, log it
+                   console.log(err);
+                 }); 
+            }
+            //if the scraped article is in the database then we end the res
+            if (data.length !== 0) {
+                res.end();
+            }
+        });
+     });
+      // Send a message to the client
+      res.send("Entertainment Scrape Complete");
+    });
+  });
+
 
 
 module.exports = router;
