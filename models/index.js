4f9498acb854616ce59e0f48b61776a580cdd3f3
williamrackley <willrackley65@gmail.com>
2019-03-23 09:54:22 -0400

    deploy

--- models/index.js
+++ models/index.js
@@ -1,5 +1,5 @@
 // Exporting an object containing all of our models
-var User = require("./User");
+//var User = require("./User");
 var Article = require("./Article");
 var Comment = require("./Comment");
 module.exports = {
